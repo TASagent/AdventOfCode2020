a5159c99d8219f18025ba8ab9a7ae56e3af26641
TASagent <TASagent.Streams@gmail.com>
2020-12-04 21:27:44 -0800

    Adding Day 5

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -11,15 +11,14 @@ namespace Day05
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5");
+            Console.WriteLine("Day 5 - Binary Boarding");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
-            string[] lines = File.ReadAllLines(inputFile);
+            List<int> seatIDs = File.ReadAllLines(inputFile).Select(TranslateSeat).ToList();
 
 
-
-            int output1 = 0;
+            int output1 = seatIDs.Max();
 
 
 
@@ -29,16 +28,45 @@ namespace Day05
             Console.WriteLine("Star 2");
             Console.WriteLine();
 
+            seatIDs.Sort();
 
             int output2 = 0;
-
-
+            for (int i = 1; i < seatIDs.Count - 2; i++)
+            {
+                if (seatIDs[i + 1] - seatIDs[i] == 2)
+                {
+                    output2 = seatIDs[i] + 1;
+                }
+            }
 
             Console.WriteLine($"The answer is: {output2}");
 
 
             Console.WriteLine();
             Console.ReadKey();
+        }
+
+        public static int TranslateSeat(string input)
+        {
+            int seatID = 0;
+
+            foreach (char c in input)
+            {
+                seatID <<= 1;
+
+                switch (c)
+                {
+                    case 'B':
+                    case 'R':
+                        seatID |= 1;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            return seatID;
         }
     }
 }
