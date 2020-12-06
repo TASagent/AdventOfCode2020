09bb5e1fc2c60d548f0ac968c9ba5f44ff2de01b
TASagent <TASagent.Streams@gmail.com>
2020-12-05 21:26:55 -0800

    Adding Day 6

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -11,17 +11,16 @@ namespace Day06
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 6");
+            Console.WriteLine("Day 6 - Custom Customs");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
-            string[] lines = File.ReadAllLines(inputFile);
+            string[] lines = File.ReadAllText(inputFile)
+                .Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
 
+            List<CustomsGroup> customGroups = lines.Select(x => new CustomsGroup(x)).ToList();
 
-
-            int output1 = 0;
-
-
+            int output1 = customGroups.Sum(x=>x.GetCount());
 
             Console.WriteLine($"The answer is: {output1}");
 
@@ -29,16 +28,37 @@ namespace Day06
             Console.WriteLine("Star 2");
             Console.WriteLine();
 
-
-            int output2 = 0;
-
-
+            int output2 = customGroups.Sum(x => x.GetEveryoneCount());
 
             Console.WriteLine($"The answer is: {output2}");
 
 
             Console.WriteLine();
             Console.ReadKey();
+        }
+
+        public class CustomsGroup
+        {
+            private HashSet<char> yesQuestions;
+
+            private HashSet<char> everyoneYes;
+
+            public CustomsGroup(string input)
+            {
+                yesQuestions = new HashSet<char>(input.Where(char.IsLetter));
+
+                string[] splitInput = input.Split("\r\n");
+
+                everyoneYes = new HashSet<char>(splitInput[0]);
+
+                for (int i = 1; i < splitInput.Length; i++)
+                {
+                    everyoneYes.IntersectWith(splitInput[i]);
+                }
+            }
+
+            public int GetCount() => yesQuestions.Count;
+            public int GetEveryoneCount() => everyoneYes.Count;
         }
     }
 }
