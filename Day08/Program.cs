b1c72a2b52b56db5942a1a0740609e1811bc8f2e
TASagent <TASagent.Streams@gmail.com>
2020-12-07 22:53:13 -0800

    Adding Day 8

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -11,34 +11,136 @@ namespace Day08
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 8");
+            Console.WriteLine("Day 8 - Handheld Halting");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
-            string[] lines = File.ReadAllLines(inputFile);
+            List<Instruction> instructions = File.ReadAllLines(inputFile).Select(x => new Instruction(x)).ToList();
 
+            int instrIndex = 0;
+            int acc = 0;
 
+            HashSet<int> visitedInstructions = new HashSet<int>();
 
-            int output1 = 0;
+            while (visitedInstructions.Add(instrIndex))
+            {
+                instructions[instrIndex].Execute(ref instrIndex, ref acc);
+            }
 
-
-
-            Console.WriteLine($"The answer is: {output1}");
+            Console.WriteLine($"The answer is: {acc}");
 
             Console.WriteLine();
             Console.WriteLine("Star 2");
             Console.WriteLine();
 
 
-            int output2 = 0;
+
+            foreach (int index in visitedInstructions)
+            {
+                HashSet<int> visitedInstructions2 = new HashSet<int>();
+
+                instrIndex = 0;
+                acc = 0;
+                bool success = false;
+
+                while (visitedInstructions2.Add(instrIndex))
+                {
+                    if (instrIndex == instructions.Count)
+                    {
+                        success = true;
+                        break;
+                    }
+
+                    if (index == instrIndex)
+                    {
+                        instructions[instrIndex].ExecuteFlipped(ref instrIndex, ref acc);
+                    }
+                    else
+                    {
+                        instructions[instrIndex].Execute(ref instrIndex, ref acc);
+                    }
+                }
+
+                if (success)
+                {
+                    break;
+                }    
+            }
 
 
-
-            Console.WriteLine($"The answer is: {output2}");
+            Console.WriteLine($"The answer is: {acc}");
 
 
             Console.WriteLine();
             Console.ReadKey();
         }
+
+        public class Instruction
+        {
+            private readonly InstrType instrType;
+            private readonly int diff;
+
+            public Instruction(string input)
+            {
+                string[] splitInput = input.Split(' ');
+
+                instrType = Enum.Parse<InstrType>(splitInput[0]);
+                diff = int.Parse(splitInput[1]);
+            }
+
+            public void Execute(ref int instrIndex, ref int acc)
+            {
+                switch (instrType)
+                {
+                    case InstrType.nop:
+                        instrIndex++;
+                        break;
+
+                    case InstrType.jmp:
+                        instrIndex += diff;
+                        break;
+
+                    case InstrType.acc:
+                        acc += diff;
+                        instrIndex++;
+                        break;
+
+                    default:
+                        throw new Exception("Not Implemented");
+                }
+            }
+
+            public void ExecuteFlipped(ref int instrIndex, ref int acc)
+            {
+                switch (instrType)
+                {
+                    case InstrType.jmp:
+                        instrIndex++;
+                        break;
+
+                    case InstrType.nop:
+                        instrIndex += diff;
+                        break;
+
+                    case InstrType.acc:
+                        acc += diff;
+                        instrIndex++;
+                        break;
+
+                    default:
+                        throw new Exception("Not Implemented");
+                }
+            }
+
+        }
+
+        public enum InstrType
+        {
+            nop,
+            acc,
+            jmp
+        }
+
+
     }
 }
