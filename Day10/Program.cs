89554621b09b6a983936c190636362630a8b71da
TASagent <TASagent.Streams@gmail.com>
2020-12-09 21:34:38 -0800

    Adding Day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -11,17 +11,46 @@ namespace Day10
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 10");
+            Console.WriteLine("Day 10 - Adapter Array");
             Console.WriteLine("Star 1");
             Console.WriteLine();
 
-            string[] lines = File.ReadAllLines(inputFile);
+            List<int> adapters = File.ReadAllLines(inputFile).Select(int.Parse).ToList();
+
+            adapters.Add(0);
+            adapters.Add(adapters.Max() + 3);
+            adapters.Sort();
+
+            //Add one at lines.Max() + 3
+
+            int oneDiffCount = 0;
+            int threeDiffCount = 0;
 
 
+            for (int i = 0; i < adapters.Count - 1; i++)
+            {
+                int diff = adapters[i + 1] - adapters[i];
 
-            int output1 = 0;
+                switch (diff)
+                {
+                    case 1:
+                        oneDiffCount++;
+                        break;
 
+                    case 2:
+                        //Do nothing
+                        break;
 
+                    case 3:
+                        threeDiffCount++;
+                        break;
+
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            int output1 = oneDiffCount * threeDiffCount;
 
             Console.WriteLine($"The answer is: {output1}");
 
@@ -29,16 +58,48 @@ namespace Day10
             Console.WriteLine("Star 2");
             Console.WriteLine();
 
+            Dictionary<int, long> cache = new Dictionary<int, long>();
 
-            int output2 = 0;
-
-
+            long output2 = GetArrangements(adapters, 0, cache);
 
             Console.WriteLine($"The answer is: {output2}");
 
-
             Console.WriteLine();
             Console.ReadKey();
+        }
+
+        static long GetArrangements(
+            List<int> adapters,
+            int currentAdapterIndex,
+            Dictionary<int, long> cache)
+        {
+            if (cache.ContainsKey(currentAdapterIndex))
+            {
+                return cache[currentAdapterIndex];
+            }
+
+            if (currentAdapterIndex == adapters.Count - 1)
+            {
+                return 1;
+            }
+
+            long arrangements = 0;
+            int currentJoltage = adapters[currentAdapterIndex];
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (currentAdapterIndex + i < adapters.Count && adapters[currentAdapterIndex + i] - currentJoltage < 4)
+                {
+                    arrangements += GetArrangements(adapters, currentAdapterIndex + i, cache);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            cache.Add(currentAdapterIndex, arrangements);
+            return arrangements;
         }
     }
 }
