4a83d52c236ccd9e18be2655e9e8d52da8e93daa
TASagent <TASagent.Streams@gmail.com>
2020-12-24 21:39:19 -0800

    Adding Day 25

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -7,20 +7,46 @@ namespace Day25
 {
     class Program
     {
-        private const string inputFile = @"../../../../input25.txt";
+        private const long InputNumber1 = 3418282;
+        private const long InputNumber2 = 8719412;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 25");
+            Console.WriteLine("Day 25 - Combo Breaker");
             Console.WriteLine();
 
-            string[] lines = File.ReadAllLines(inputFile);
+            long doorSecretNumber = 0;
+            long cardSecretNumber = 0;
 
+            long subjectNumber = 7;
+            long currentNumber = 1;
 
+            while (currentNumber != InputNumber1)
+            {
+                currentNumber *= subjectNumber;
+                currentNumber %= 20201227;
+                doorSecretNumber++;
+            }
 
-            int output = 0;
+            currentNumber = 1;
 
+            while (currentNumber != InputNumber2)
+            {
+                currentNumber *= subjectNumber;
+                currentNumber %= 20201227;
+                cardSecretNumber++;
+            }
 
+            subjectNumber = InputNumber1;
+            currentNumber = 1;
+
+            for (int loop = 0; loop < cardSecretNumber; loop++)
+            {
+                currentNumber *= subjectNumber;
+                currentNumber %= 20201227;
+            }
+
+            long output = currentNumber;
 
             Console.WriteLine($"The answer is: {output}");
             Console.WriteLine();
